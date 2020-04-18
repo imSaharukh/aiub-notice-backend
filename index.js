f88bdce610a301c85adfdb4cf89912f64bd9a956
imSaharukh <sasaharukh@gmail.com>
2020-04-18 23:58:50 +0600

    express added

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const puppeteer = require('puppeteer');
+const express = require('express');
+var app = express();
+const port = 3000
+app.get('/', function (req, res) {
+    (async () => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto('https://www.aiub.edu/category/notices?pageNo=1&pageSize=20');
+        //await page.screenshot({ path: 'example.png' });
 
-(async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://www.aiub.edu/category/notices?pageNo=1&pageSize=20');
-    //await page.screenshot({ path: 'example.png' });
+        const result = await page.evaluate(() => {
+            let titleFromAIUB = document.querySelectorAll(".title")
+            const titleList = [...titleFromAIUB]
+            return titleList.map(h => h.innerText)
+        })
+        console.log(result);
+        res.send(result)
+        await browser.close();
+    })();
 
-    const result = await page.evaluate(() => {
-        let titleFromAIUB = document.querySelectorAll(".title")
-        const titleList = [...titleFromAIUB]
-        return titleList.map(h => h.innerText)
-    })
-    console.log(result);
-    await browser.close();
-})();
+})
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
