394b0dc1627817af6c22a10b4501682391847917
imSaharukh <sasaharukh@gmail.com>
2020-04-20 22:18:13 +0600

    working

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 const puppeteer = require('puppeteer');
-const express = require('express');
-var app = express();
-const port = 3000
-app.get('/', function (req, res) {
-    (async () => {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto('https://www.aiub.edu/category/notices?pageNo=1&pageSize=20');
-        //await page.screenshot({ path: 'example.png' });
+//express installed
 
-        const result = await page.evaluate(() => {
-            let titleFromAIUB = document.querySelectorAll(".title")
-            const titleList = [...titleFromAIUB]
-            return titleList.map(h => h.innerText)
-        })
-        console.log(result);
-        res.send(result)
-        await browser.close();
-    })();
+let titles;
+let links
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.aiub.edu/category/notices?pageNo=1&pageSize=20');
+    //await page.screenshot({ path: 'example.png' });
 
-})
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+    titles = await page.evaluate(() => {
+        let titleFromAIUB = document.querySelectorAll(".title")
+        const titleList = [...titleFromAIUB]
+        return titleList.map(h => h.innerText)
+    })
+    links = await page.evaluate(() => {
+        let linkFromAIUB = document.querySelectorAll(".info-link")
+        const linkList = [...linkFromAIUB]
+        return linkList.map(h => h.href)
+    })
+
+    console.log(titles)
+    console.log(links)
+    await browser.close();
+})();
+
+
 
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+})
+// app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
